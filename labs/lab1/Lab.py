6c941556357ed47562d419450c424527eeec4bbd
Каратаев Данил <Dan257@stud.vvsu.ru>
2025-09-23 13:03:33 +1000

    Add timetable23

--- labs/lab1/Lab.py
+++ labs/lab1/Lab.py
@@ -1,64 +1,64 @@
-#Num1
-# a1 = 231
-# b1 = 24.2
-# c1 = 'fdas'
-# d1 = True
+Num1
+a1 = 231
+b1 = 24.2
+c1 = 'fdas'
+d1 = True
 
-#Num2
-# a2 = 'Danil'
-# b2 = 17
-# print(a2,b2)
+Num2
+a2 = 'Danil'
+b2 = 17
+print(a2,b2)
 
-#Num3
-# a3 = 342
-# b3 = 56.2
-# c3 = '43'
-# d3 = float(a3) + b3 + float(c3)
-# print(d3)
+Num3
+a3 = 342
+b3 = 56.2
+c3 = '43'
+d3 = float(a3) + b3 + float(c3)
+print(d3)
 
-#Num4
-# a4 = 3
-# b4 = 8
-# print(((a4 + (4 * b4)) * (a4 - (3 * b4))) + (a4**2))
+Num4
+a4 = 3
+b4 = 8
+print(((a4 + (4 * b4)) * (a4 - (3 * b4))) + (a4**2))
 
-#Num5
-# print(int(input('')) * int(input('')))
+Num5
+print(int(input('')) * int(input('')))
 
-#Num6
-# print('*   *   *')
-# print(' * * * *')
-# print('  *  * ')
+Num6
+print('*   *   *')
+print(' * * * *')
+print('  *  * ')
 
-#Num7
-# a7 = 5
-# b7 = 3
-# print(a7 + b7, a7 - b7, a7 * b7, a7 / b7, a7 ** b7, a7 // b7, a7 % b7)
-# print(a7 > b7, a7 < b7, a7 <= b7, a7 >= b7, a7 == b7, a7 != b7)
+Num7
+a7 = 5
+b7 = 3
+print(a7 + b7, a7 - b7, a7 * b7, a7 / b7, a7 ** b7, a7 // b7, a7 % b7)
+print(a7 > b7, a7 < b7, a7 <= b7, a7 >= b7, a7 == b7, a7 != b7)
 
-#Num8
-# a8 = 'Данил'
-# b8 = '17'
-# print(f'Меня зовут {a8} , мне {b8} лет.')
+Num8
+a8 = 'Данил'
+b8 = '17'
+print(f'Меня зовут {a8} , мне {b8} лет.')
 
-#Num9
-# a9 = 'Съешь ещё '
-# b9 = 'этих мягких '
-# c9 = 'французких булок, '
-# d9 = 'да выпей '
-# e9 = 'чаю.'
-# print(a9 + b9 + c9 + d9 + e9)
+Num9
+a9 = 'Съешь ещё '
+b9 = 'этих мягких '
+c9 = 'французких булок, '
+d9 = 'да выпей '
+e9 = 'чаю.'
+print(a9 + b9 + c9 + d9 + e9)
 
-#Num10
-# print('Нет! Да! ' * 4)
+Num10
+print('Нет! Да! ' * 4)
 
-#Num11
-# a11 = int(input(''))
-# b11 = int(input(''))
-# c11 = int(input(''))
-# d11 = (a11 + c11) // b11
-# print('Результат вычисления: ', d11)
+Num11
+a11 = int(input(''))
+b11 = int(input(''))
+c11 = int(input(''))
+d11 = (a11 + c11) // b11
+print('Результат вычисления: ', d11)
 
-#Num12
+Num12
 a12 = input()
 print(a12[:4])
 print(a12[8:10])
