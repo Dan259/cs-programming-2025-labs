2507588f4dc5632282039f5b8b4850ebda45178f
Dan259 <156999793+Dan259@users.noreply.github.com>
2025-09-29 18:51:21 +1000

    Update Lab.py

--- labs/lab1/Lab.py
+++ labs/lab1/Lab.py
@@ -1,66 +1,67 @@
-Num1
-a1 = 231
-b1 = 24.2
-c1 = 'fdas'
-d1 = True
+def num1():
+    a1 = 231
+    b1 = 24.2
+    c1 = 'fdas'
+    d1 = True
 
-Num2
-a2 = 'Danil'
-b2 = 17
-print(a2,b2)
+def num2():
+    a2 = 'Danil'
+    b2 = 17
+    print(a2,b2)
 
-Num3
-a3 = 342
-b3 = 56.2
-c3 = '43'
-d3 = float(a3) + b3 + float(c3)
-print(d3)
+def num3():
+    a3 = 342
+    b3 = 56.2
+    c3 = '43'
+    d3 = float(a3) + b3 + float(c3)
+    print(d3)
 
-Num4
-a4 = 3
-b4 = 8
-print(((a4 + (4 * b4)) * (a4 - (3 * b4))) + (a4**2))
+def num4():
+    a4 = 3
+    b4 = 8
+    print(((a4 + (4 * b4)) * (a4 - (3 * b4))) + (a4**2))
 
-Num5
-print(int(input('')) * int(input('')))
+def num5():
+    print(int(input('')) * int(input('')))
 
-Num6
-print('*   *   *')
-print(' * * * *')
-print('  *  * ')
+def num6():
+    print('*   *   *')
+    print(' * * * *')
+    print('  *  * ')
 
-Num7
-a7 = 5
-b7 = 3
-print(a7 + b7, a7 - b7, a7 * b7, a7 / b7, a7 ** b7, a7 // b7, a7 % b7)
-print(a7 > b7, a7 < b7, a7 <= b7, a7 >= b7, a7 == b7, a7 != b7)
+def num7():
+    a7 = 5
+    b7 = 3
+    print(a7 + b7, a7 - b7, a7 * b7, a7 / b7, a7 ** b7, a7 // b7, a7 % b7)
+    print(a7 > b7, a7 < b7, a7 <= b7, a7 >= b7, a7 == b7, a7 != b7)
 
-Num8
-a8 = 'Данил'
-b8 = '17'
-print(f'Меня зовут {a8} , мне {b8} лет.')
+def num8():
+    a8 = 'Данил'
+    b8 = '17'
+    print(f'Меня зовут {a8} , мне {b8} лет.')
 
-Num9
-a9 = 'Съешь ещё '
-b9 = 'этих мягких '
-c9 = 'французких булок, '
-d9 = 'да выпей '
-e9 = 'чаю.'
-print(a9 + b9 + c9 + d9 + e9)
+def num9():
+    a9 = 'Съешь ещё '
+    b9 = 'этих мягких '
+    c9 = 'французких булок, '
+    d9 = 'да выпей '
+    e9 = 'чаю.'
+    print(a9 + b9 + c9 + d9 + e9)
 
-Num10
-print('Нет! Да! ' * 4)
+def num10():
+    print('Нет! Да! ' * 4)
 
-Num11
-a11 = int(input(''))
-b11 = int(input(''))
-c11 = int(input(''))
-d11 = (a11 + c11) // b11
-print('Результат вычисления: ', d11)
+def num11():
+    e11 = int(input(''))
+    a11, b11, c11 = e11.split(',')
+    d11 = (a11 + c11) // b11
+    print('Результат вычисления: ', d11)
 
-Num12
-a12 = input()
-print(a12[:4])
-print(a12[8:10])
-print(a12[4:8])
-print(a12[::-1])
+def num12():
+    a12 = input()
+    print(a12[:4])
+    print(a12[8:10])
+    print(a12[4:8])
+    print(a12[::-1])
+    
+num11
