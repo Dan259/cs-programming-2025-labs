74ea758004077f2b7933db400d280f275e8407c7
Каратаев Данил <Dan257@stud.vvsu.ru>
2025-12-02 11:58:31 +1000

    eee

--- labs/Mathlog/mathlog.py
+++ labs/Mathlog/mathlog.py
@@ -1,9 +1,21 @@
-formuls = input('Введите формулы:').split(',')
-kol = len(folmuls)
-if kol == 2:
-    if formuls[0] in formuls[1][:formuls[1].find('<')]:
-        if '<=' in formuls[1]:
-            print('Заключение:')
-            print(formuls[1].replace(formuls[0] + ' <='))
+while True:
+    otvet = []
+    a = input('Введите 5 натуральных чисел через пробел:').split()
+    print(a)
+    if len(a) == 5:
+        for i in range(len(a)):
+            if not(a[i].replace('-','').isdigit()):
+                print("Введено не число")
+                break
         else:
-            print('Нет modus ponens')
+            a = sorted(list(map(int,a)))
+            if a[0] > 0:
+                for x in range(5):
+                    for y in range(x,5):
+                        if x < y and f'({a[x]}, {a[y]})' not in otvet and a[x] != a[y]:
+                            otvet.append(f'({a[x]}, {a[y]})')
+            else:
+                print('Введено не натуральное число')
+            print(otvet)
+    else:
+        print('Нужно ввести 5 чисел')
