c8a0f7a0e1d1c9b23597c1d9575c68107c024030
Dan259 <156999793+Dan259@users.noreply.github.com>
2025-12-08 16:28:57 +1000

    6лаба

--- labs/lab6/1.py
+++ labs/lab6/1.py
@@ -1,1 +1,20 @@
-print(235)
+a = input('Введите время:').split()
+a.append(a[0][len(a[0])-1])
+a[0] = a[0][:len(a[0])-1]
+ans = 0
+if a[2] == 'm':
+    if a[1] == 'h':
+        ans = float(a[0]) / 60
+    elif a[1] == 's':
+        ans = float(a[0]) * 60
+elif a[2] == 's':
+    if a[1] == 'm':
+        ans = float(a[0]) / 60
+    elif a[1] == 'h':
+        ans = float(a[0]) / 3600
+elif a[2] == 'h':
+    if a[1] == 'm':
+        ans = float(a[0]) * 60
+    elif a[1] == 's':
+        ans = float(a[0]) * 3600
+print(ans,a[1])
--- labs/lab6/2.py
+++ labs/lab6/2.py
@@ -1,1 +1,17 @@
-print('Hello world!')
+a = input('Введите сумму и количество лет:').split()
+b = 0
+income = 0
+for i in range(int(a[1])):
+    a1 = int(a[0])
+    while b <= 4.7 and a1 >= 9999:
+        a1 -= 10000
+        b += 0.3
+        b = round(b,1)
+    if i <= 3: b += 3
+    elif 4 <= i <= 6: b += 5
+    else: b += 2
+    income += int(a[0]) * (b / 100)
+    a[0] = int(a[0]) + income
+    print(b,a[0],income)
+    b = 0
+print(income)
