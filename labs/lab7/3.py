1b0a35f0ee7f0b853dd10758eb537b2179126c4e
Dan259 <156999793+Dan259@users.noreply.github.com>
2025-12-28 20:21:53 +1000

    fgh

---
+++ labs/lab7/3.py
@@ -0,0 +1,15 @@
+personnel = [
+    {"name": "Dr. Klein", "clearance": 2},
+    {"name": "Agent Brooks", "clearance": 4},
+    {"name": "Technician Reed", "clearance": 1}
+]
+a = list(map(lambda x: {
+    **x,
+    "category": (
+        "Restricted" if x["clearance"] == 1 else
+        "Confidential" if 2 <= x["clearance"] <= 3 else
+        "Top Secret"
+    )
+}, personnel))
+
+print(a)
